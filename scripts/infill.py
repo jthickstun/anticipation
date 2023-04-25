9ef950bcd6127845054f6e38d3c4bdd51e12df96
John Thickstun <jthickstun@jagupard13.stanford.edu>
2023-04-25 15:22:28 -0700

    more harmonization setup updates

--- scripts/infill.py
+++ scripts/infill.py
@@ -8,6 +8,7 @@ import numpy as np
 from transformers import GPT2LMHeadModel, AutoModelForCausalLM
 
 from anticipation import ops
+from anticipation.visuals import visualize
 from anticipation.sample import generate, generate_ar
 from anticipation.tokenize import extract_instruments
 from anticipation.convert import midi_to_events, events_to_midi
@@ -15,8 +16,6 @@ from anticipation.config import TIME_RESOLUTION
 from anticipation.vocab import TIME_OFFSET, NOTE_OFFSET
 
 np.random.seed(0)
-#with open(DATA, 'r') as f:
-#    all_tokens = f.readlines()
 
 def select_sample(filenames, prompt_length, clip_length, the_idx=None):
     while True:
@@ -42,8 +41,11 @@ def select_sample(filenames, prompt_length, clip_length, the_idx=None):
         clip = ops.translate(clip, -int(TIME_RESOLUTION*start_time))
 
         # find an ensemble with a healthy (non-drum / effect) instrument collection in the prompt
-        instruments = ops.get_instruments(ops.clip(events, 0, prompt_length)).keys()
-        if len([instr for instr in instruments if instr < 112]) < 4:
+        #instruments = ops.get_instruments(ops.clip(events, 0, prompt_length)).keys()
+        #if len([instr for instr in instruments if instr < 112]) < 4:
+        #    continue
+        instruments = ops.get_instruments(events).keys()
+        if len([instr for instr in instruments if instr != 128]) != 4:
             continue
 
         # define melody as the intstrument part with the highest (non-drum, non-piano) pitchj
@@ -85,17 +87,12 @@ def select_sample(filenames, prompt_length, clip_length, the_idx=None):
 
 
 def main(args):
+    print(f'Harmonizing with tracks sampled from: {args.dir}')
     filenames = glob(args.dir + '/**/*.mid', recursive=True) \
             + glob(args.dir + '/**/*.midi', recursive=True)
     filenames = sorted(filenames)
 
-    print(f'Harmonizing with tracks sampled from: {args.dir}')
-
-
     print(f'Harmonizing using model checkpoint: {args.model}')
-    #CHECKPOINT= "/nlp/scr/jthickstun/anticipation/checkpoints/jumping-jazz-234/step-100000/hf"
-    #CHECKPOINT="/nlp/scr/jthickstun/anticipation/checkpoints/dainty-elevator-270/step-200000/hf"
-
     t0 = time.time()
     model = AutoModelForCausalLM.from_pretrained(args.model).cuda()
     print(f'Loaded model ({time.time()-t0} seconds)')
@@ -108,41 +105,63 @@ def main(args):
 
     for i in range(args.count):
         tokens, melody, idx = select_sample(filenames, args.prompt_length, args.clip_length)
-        filename = f'output/original-{i}.mid'
-        mid = events_to_midi(ops.clip(tokens, 0, args.clip_length))
-        mid.save(filename)
+        clip = ops.clip(tokens, 0, args.clip_length)
+        mid = events_to_midi(clip)
+        mid.save(f'output/clip-{i}.mid')
+        if args.visualize:
+            visualize(clip, f'output/clip-{i}.png')
+
+        full_prompt = ops.clip(tokens, 0, args.prompt_length)
+        mid = events_to_midi(full_prompt)
+        mid.save(f'output/prompt-{i}.mid')
+        if args.visualize:
+            visualize(full_prompt, f'output/prompt-{i}.png')
 
         tokens, labels = extract_instruments(tokens, [melody])
         prompt = ops.clip(tokens, 0, args.prompt_length, clip_duration=False)
 
-        filename = f'output/conditional-{i}.mid'
         mid = events_to_midi(ops.clip(prompt + labels, 0, args.clip_length))
-        mid.save(filename)
+        mid.save(f'output/conditional-{i}.mid')
+        if args.visualize:
+            visualize(ops.clip(ops.combine(prompt, labels), 0, args.clip_length), f'output/conditional-{i}.png')
 
         for j in range(args.multiplicity):
             t0 = time.time()
             if args.baseline:
-                filename = f'output/generated-ar-{i}-v{j}.mid'
                 generated_tokens = generate_ar(model, args.prompt_length, args.clip_length, prompt, labels, top_p=.98)
-                mid = events_to_midi(ops.clip(generated_tokens, 0, args.clip_length))
-                mid.save(filename)
+                output = ops.clip(generated_tokens, 0, args.clip_length)
+                mid = events_to_midi(output)
+                mid.save(f'output/generated-ar-labels-{i}-v{j}.mid')
+                if args.visualize:
+                    visualize(output, f'output/generated-ar-labels-{i}-v{j}.png')
 
             if args.retrieve:
-                filename = f'output/generated-retrieval{i}-v{j}.mid'
-
                 generated_tokens, _, _ = select_sample(filenames, args.prompt_length, args.clip_length, idx)
                 generated_tokens, _ = extract_instruments(generated_tokens, [melody])
                 generated_tokens = ops.clip(generated_tokens, args.prompt_length, args.clip_length, clip_duration=False)
-                mid = events_to_midi(ops.clip(prompt + labels + generated_tokens, 0, args.clip_length))
-                mid.save(filename)
+                output = ops.clip(ops.combine(prompt + generated_tokens, labels), 0, args.clip_length)
+                mid = events_to_midi(output)
+                mid.save(f'output/generated-retrieval{i}-v{j}.mid')
+                if args.visualize:
+                    visualize(output, f'output/generated-retrieval{i}-v{j}.png')
+
+            if args.completion:
+                generated_tokens = generate(model, args.prompt_length, args.clip_length, full_prompt, [], top_p=.98, debug=False)
+                output = ops.clip(generated_tokens, 0, args.clip_length)
+                mid = events_to_midi(output)
+                mid.save(f'output/generated-ar-{i}-v{j}.mid')
+                if args.visualize:
+                    visualize(output, f'output/generated-ar-{i}-v{j}.png')
 
             if args.anticipatory:
-                filename = f'output/generated-aar-{i}-v{j}.mid'
                 generated_tokens = generate(model, args.prompt_length, args.clip_length, prompt, labels, top_p=.98, debug=False)
-                mid = events_to_midi(ops.clip(generated_tokens + labels, 0, args.clip_length))
-                mid.save(filename)
+                output = ops.clip(ops.combine(generated_tokens, labels), 0, args.clip_length)
+                mid = events_to_midi(output)
+                mid.save(f'output/generated-aar-{i}-v{j}.mid')
+                if args.visualize:
+                    visualize(output, f'output/generated-aar-{i}-v{j}.png')
 
-            print(f'Harmonized with {melody}. Sampling time: {time.time()-t0} seconds')
+            print(f'Harmonized with instrument {melody}. Sampling time: {time.time()-t0} seconds')
 
 
 if __name__ == '__main__':
@@ -161,6 +180,10 @@ if __name__ == '__main__':
             help='generate anticipatory results')
     parser.add_argument('-b', '--baseline', action='store_true',
             help='generate autoregressive (baseline) results')
+    parser.add_argument('-u', '--completion', action='store_true',
+            help='generate autoregressive completion results')
     parser.add_argument('-r', '--retrieve', action='store_true',
             help='generate the retrieval baseline')
+    parser.add_argument('-v', '--visualize', action='store_true',
+            help='plot visualizations')
     main(parser.parse_args())
