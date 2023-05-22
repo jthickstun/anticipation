3ceb98d95f270a6e8e01e987cfb81693bbaadad8
John Thickstun <jthickstun@john1.stanford.edu>
2023-05-21 18:54:23 -0700

    module docstring

---
+++ anticipation/__init__.py
@@ -0,0 +1,9 @@
+""" Infrastructure for constructing anticipatory infilling models.
+
+This model provides infrastructure to preprocess Midi music datasets
+for training anticipatory music infilling models. For more context, see:
+
+    Anticipatory Music Transformer
+    John Thickstun, David Hall, Chris Donahue, Percy Liang
+    Preprint Report, 2023
+"""
