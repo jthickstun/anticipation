fd3bf1ff4f6cf88488e8639212d8d2dbf46a0438
caenopy <nickpbecker@gmail.com>
2024-01-28 22:53:24 -0800

    add flag for generating chord track:

--- train/midi-preprocess.py
+++ train/midi-preprocess.py
@@ -11,9 +11,9 @@ from anticipation.config import PREPROC_WORKERS
 from anticipation.vocabs.tripletmidi import vocab
 
 
-def convert_midi(filename, debug=False):
+def convert_midi(filename, harmonize, debug=False):
     try:
-        tokens = midi_to_compound(filename, vocab, debug=debug)
+        tokens = midi_to_compound(filename, vocab, harmonize, debug=debug)
     except Exception:
         if debug:
             print('Failed to process: ', filename)
@@ -31,10 +31,11 @@ def main(args):
     print(f'Midi time quantization is: {vocab["config"]["midi_quantization"]}')
     filenames = glob(args.dir + '/**/*.mid', recursive=True) \
             + glob(args.dir + '/**/*.midi', recursive=True)
-
+    
+    harmonize = args.harmonize
     print(f'Preprocessing {len(filenames)} files with {PREPROC_WORKERS} workers')
     with ProcessPoolExecutor(max_workers=PREPROC_WORKERS) as executor:
-        results = list(tqdm(executor.map(convert_midi, filenames), desc='Preprocess', total=len(filenames)))
+        results = list(tqdm(executor.map(convert_midi, filenames, harmonize), desc='Preprocess', total=len(filenames)))
 
     discards = round(100*sum(results)/float(len(filenames)),2)
     print(f'Successfully processed {len(filenames) - sum(results)} files (discarded {discards}%)')
@@ -42,4 +43,5 @@ def main(args):
 if __name__ == '__main__':
     parser = ArgumentParser(description='prepares a MIDI dataset')
     parser.add_argument('dir', help='directory containing .mid files for training')
+    parser.add_argument('--harmonize', type=bool, default=False, help="harmonize and store chords with program code specified by vocab")
     main(parser.parse_args())
