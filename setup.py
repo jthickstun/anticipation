d1b0f5318d30e2f379ab29c3bae882be01a3b05d
caenopy <nickpbecker@gmail.com>
2024-02-02 15:42:53 -0800

    version control

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='anticipation',
-   version='1.0',
+   version='1.1',
    description='Anticipatory Autoregressive Models',
    author='John Thickstun',
    author_email='jthickstun@cs.stanford.edu',
