c7f16f792b80072c74f5cd0971bf7cd264094625
caenopy <nickpbecker@gmail.com>
2024-02-04 12:52:19 -0800

    debugging off as default and PREPROC_WORKERS as # workers

--- train/midi-preprocess.py
+++ train/midi-preprocess.py
@@ -13,7 +13,7 @@ from anticipation.vocabs.tripletmidi import vocab
 import os
 
 
-def convert_midi(filename, harmonize, output=None, debug=True):
+def convert_midi(filename, harmonize, output=None, debug=False):
     try:
         if debug:
             print('Processing file: ', filename)
@@ -55,8 +55,8 @@ def main(args):
         output = args.output
     else:
         output = None
-    print(f'Preprocessing {len(filenames)} files with {1} workers')
-    with ProcessPoolExecutor(max_workers=1) as executor:
+    print(f'Preprocessing {len(filenames)} files with {PREPROC_WORKERS} workers')
+    with ProcessPoolExecutor(max_workers=PREPROC_WORKERS) as executor:
         partial_convert_midi = partial(convert_midi, harmonize=harmonize, output=output)
         results = list(tqdm(executor.map(partial_convert_midi, filenames), desc='Preprocess', total=len(filenames)))
 
